95b8dc5a676f3b74c125a7d657b495a324c7c62e
user <user@AJIT>
2025-03-27 23:20:33 +0530

    Add project files.

---
+++ LeaveManagementSystem.Web/Controllers/TestController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using LeaveManagementSystem.Web.Models;
+
+namespace LeaveManagementSystem.Web.Controllers
+{
+    public class TestController : Controller
+    {
+        public IActionResult Index()
+        {
+            var data = new TestViewModel
+            {
+                Name = "Ajitkumar Govinbdaraj",
+                Dob = new DateTime(1997, 11, 08)
+            };
+
+            return View(data);
+        }
+    }
+}
